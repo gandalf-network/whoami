09e14cdf0b80c45f428302ce69a0634db82c8a7d
TosinJs <tosinjsamuel@gmail.com>
2024-04-03 19:44:15 +0100

    Update

---
+++ src/actions/database/index.ts
@@ -0,0 +1,7 @@
+export async function getStats(sessionID: string) {}
+
+export async function getReportCard(sessionID: string) {}
+
+export async function getSession(sessionID: string) {}
+
+export async function startSession(sessionID: string) {}
---
+++ src/actions/eyeofsauron/index.ts
@@ -0,0 +1,8 @@
+// TypeScript file generated from C:\Users\user\AppData\Roaming\npm\node_modules\@gandalf-network\eyeofsauron\dist\scripts\templates\index.txt 
+ import Eye, { Source } from "./gql/__generated__";
+
+import { GandalfError, GandalfErrorCode } from "./errors";
+
+export { Source, GandalfError, GandalfErrorCode };
+
+export default Eye;

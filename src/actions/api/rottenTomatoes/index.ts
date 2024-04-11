1a9227e3fbd6c5e59109ffd4ba2bf76b4b8ce28c
TosinJs <tosinjsamuel@gmail.com>
2024-04-11 06:06:51 +0100

    Update URL

--- src/actions/api/rottenTomatoes/index.ts
+++ src/actions/api/rottenTomatoes/index.ts
@@ -1,10 +1,12 @@
 import puppeteer from "puppeteer";
 
+import { ROTTEN_TOMATOES_BASE_URL } from "@/actions/helpers/constants";
+
 export async function getRottenTomatoScore(title: string) {
   title = title.replaceAll(" ", "_");
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
-  await page.goto(title, {
+  await page.goto(`${ROTTEN_TOMATOES_BASE_URL}/tv/${title}`, {
     waitUntil: "networkidle2",
     timeout: 1000000,
   });

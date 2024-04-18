40a03d810d440a90929566abff9c27bcabc79e28
TosinJs <tosinjsamuel@gmail.com>
2024-04-18 02:29:37 +0100

    Update Rotten Tomato Process

--- src/actions/helpers/constants.ts
+++ src/actions/helpers/constants.ts
@@ -6,3 +6,8 @@ export const TVDB_BASE_URL = process.env.TVDB_BASE_URL as string;
 export const OPENAI_API_KEY = process.env.OPENAI_API_KEY as string;
 export const ROTTEN_TOMATOES_BASE_URL = process.env
   .ROTTEN_TOMATOES_BASE_URL as string;
+
+export const ROTTEN_TOMATOES_API_KEY = process.env
+  .ROTTEN_TOMATOES_X_AGOLIA_API_KEY as string;
+export const ROTTEN_TOMATOES_APPLICATION_ID = process.env
+  .ROTTEN_TOMATOES_X_AGOLIA_APPLICATION_ID as string;

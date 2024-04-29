760814ebb240185c97c3b45210cceb08891672eb
TosinJs <tosinjsamuel@gmail.com>
2024-04-29 13:37:18 +0100

    Update Serverless Functions

---
+++ src/app/api/activities/queryActivities/queryActivities.ts
@@ -0,0 +1,28 @@
+import { getAndDumpActivities } from "@/actions";
+
+export async function POST(req: Request) {
+  const { sessionID, dataKey } = (await req.json()) as {
+    sessionID: string;
+    dataKey: string;
+  };
+  try {
+    await getAndDumpActivities(sessionID, dataKey);
+    return new Response(
+      JSON.stringify({ message: "Job processed successfully" }),
+      {
+        status: 200,
+        headers: {
+          "Content-Type": "application/json",
+        },
+      },
+    );
+  } catch (error) {
+    console.error("Error processing job:", error);
+    return new Response(JSON.stringify({ message: "Error processing job" }), {
+      status: 500,
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+  }
+}

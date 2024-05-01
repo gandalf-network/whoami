384225221bff7b4d7feb414fa49396409e7cee60
Ayodeji Akinola <akinayodeji4all@gmail.com>
2024-05-01 23:19:27 +0100

    fix: worker deployment

--- server.ts
+++ server.ts
@@ -15,28 +15,14 @@ instantiateWorkers();
 
 // REF: https://nextjs.org/docs/pages/building-your-application/configuring/custom-server
 app.prepare().then(() => {
-  createServer(async (req: IncomingMessage, res: ServerResponse) => {
-    try {
-      const parsedUrl = parse(req.url as string, true)
-      const { pathname, query } = parsedUrl
-      if (pathname === '/a') {
-        await app.render(req, res, '/a', query)
-      } else if (pathname === '/b') {
-        await app.render(req, res, '/b', query)
-      } else {
-        await handle(req, res, parsedUrl)
-      }
-    } catch (err) {
-      console.error('Error occurred handling', req.url, err)
-      res.statusCode = 500
-      res.end('internal server error')
-    }
-  })
-    .once('error', (err: any) => {
-      console.error(err)
-      process.exit(1)
-    })
-    .listen(port, () => {
-      console.log(`> Ready on http://${hostname}:${port}`)
-    })
-})
+  createServer((req, res) => {
+    const parsedUrl = parse(req.url!, true);
+    handle(req, res, parsedUrl);
+  }).listen(port);
+
+  console.log(
+    `> Server listening at http://localhost:${port} as ${
+      dev ? "development" : process.env.NODE_ENV
+    }`,
+  );
+});

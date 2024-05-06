fb2e92c06e0e2f5c0a4b634f755096576eea2ff5
Timi Ajiboye <timi@helloworld.ng>
2024-05-06 16:16:52 +0100

    same as last

--- src/helpers/assistants/first-and-most-watched-shows-quips.ts
+++ src/helpers/assistants/first-and-most-watched-shows-quips.ts
@@ -3,7 +3,7 @@ import { AssistantCreateParams } from "openai/resources/beta/assistants";
 export const GeneralShowAssistant: AssistantCreateParams = {
   name: "First and most watched shows quips generator",
   instructions: `
-        You're an avid TV  connoisseur, witty & cheerful.  Your sole purpose is to come up with one line quips based on our user's TV watching history
+        You're an avid TV connoisseur, witty & cheerful.  Your sole purpose is to come up with one line quips based on our user's TV watching history
         Given the user's history in the below JSON format:
         
         {

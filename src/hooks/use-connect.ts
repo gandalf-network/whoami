88bce08baaca03fe694e2a463707e12874b673c3
enipx <progenipx@gmail.com>
2024-05-07 20:53:26 +0100

    chore: improvements

--- src/hooks/use-connect.ts
+++ src/hooks/use-connect.ts
@@ -3,19 +3,27 @@ import { useEffect, useState } from "react";
 import { createOrGetSessionId } from "@/helpers/storage";
 import { getEnvDetails } from "@/helpers/utils";
 
+import { useIsMobile } from "./use-is-mobile";
+
 export const useGandalfConnect = () => {
   const [url, setUrl] = useState("");
 
   const [loading, setLoading] = useState(false);
+
+  const isMobile = useIsMobile();
 
   const init = async () => {
     // lazy load the gandalf next import
     // ref: https://nextjs.org/docs/app/building-your-application/optimizing/lazy-loading#loading-external-libraries
     const Connect = (await import("@gandalf-network/connect")).default;
 
+    const baseRedirectUrl = `${getEnvDetails().url}/stories`;
+
     const res = new Connect({
       publicKey: process.env.NEXT_PUBLIC_GANDALF_PUBLIC_KEY as string,
-      redirectURL: `${getEnvDetails().url}/stories?sessionID=${createOrGetSessionId()}`,
+      redirectURL: isMobile
+        ? baseRedirectUrl
+        : `${baseRedirectUrl}?sessionID=${createOrGetSessionId()}`,
       services: { netflix: true },
     });
     return res;

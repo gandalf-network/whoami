a9135ecb7fac1542170a8806f9b809636bc8f3aa
enipx <progenipx@gmail.com>
2024-05-07 10:04:35 +0100

    chore: staging env

--- src/helpers/base.ts
+++ src/helpers/base.ts
@@ -1,5 +1,7 @@
 // this is a utility function that checks if the current environment is production
-export const isProduction = () => process.env.NODE_ENV === "production";
+export const isProduction = () =>
+  process.env.NODE_ENV === "production" ||
+  process.env.NEXT_PUBLIC_VERCEL_ENV === "production";
 
 // this is a utility function that appends the protocol to the url
 export const appendProtocolToUrl = (url: string) => {
@@ -10,14 +12,26 @@ export const appendProtocolToUrl = (url: string) => {
 
 // this is a utility function that gets the environment details
 export const getEnvDetails = () => {
-  const vercelUrl =
+  // this is the preview url
+  const previewUrl =
     process.env.NEXT_PUBLIC_VERCEL_BRANCH_URL ||
     process.env.NEXT_PUBLIC_VERCEL_URL ||
     "";
 
+  // the staging details and url
+  const isStaging = previewUrl.includes("staging");
+
+  const stagingUrl = process.env.NEXT_PUBLIC_STAGING_URL || previewUrl;
+
+  // production url
+  const productionUrl =
+    process.env.NEXT_PUBLIC_VERCEL_PROJECT_PRODUCTION_URL || previewUrl;
+
   const url = isProduction()
-    ? vercelUrl
-    : vercelUrl || process.env.NEXT_PUBLIC_APP_URL || "";
+    ? productionUrl
+    : isStaging
+      ? stagingUrl
+      : previewUrl || process.env.NEXT_PUBLIC_APP_URL;
 
   const sterilizeUrl = url ? new URL(appendProtocolToUrl(url)) : undefined;
 

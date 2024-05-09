06f21ca291a6d241086e1aa888c82d59f82f565b
Ayodeji Akinola <akinayodeji4all@gmail.com>
2024-05-09 18:12:58 +0100

    test test

--- src/actions/inngest/functions/stateThresholdCheck.ts
+++ src/actions/inngest/functions/stateThresholdCheck.ts
@@ -28,7 +28,6 @@ export const stateThresholdCheckTask = inngest.createFunction(
   async ({ event, step }) => {
     const { sessionID } = event.data;
     if (!sessionID) return;
-    return;
 
     console.log(`> Running state threshold checks... SID: ${sessionID}`);
 

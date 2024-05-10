ada967977b78c657296716f72ce0988a98b3741b
Ayodeji Akinola <akinayodeji4all@gmail.com>
2024-05-10 16:28:52 +0100

    fix migration

--- prisma/migrations/20240509220110_change_state_to_processing/migration.sql
+++ prisma/migrations/20240509220110_change_state_to_processing/migration.sql
@@ -1,16 +1,18 @@
-/*
-  Warnings:
-
-  - The values [CRUNCHING_DATA] on the enum `UserState` will be removed. If these variants are still used in the database, this will fail.
-
-*/
--- AlterEnum
+-- Start the transaction
 BEGIN;
-CREATE TYPE "UserState_new" AS ENUM ('AWAITING_CONNECTION', 'CONNECTED', 'FAILED', 'PROCESSING', 'STATS_DATA_READY', 'COMPLETED');
+CREATE TYPE "UserStateNew" AS ENUM ('AWAITING_CONNECTION', 'CONNECTED', 'FAILED', 'PROCESSING', 'STATS_DATA_READY', 'COMPLETED');
+-- Update the user state column to use the new type and map CRUNCHING_DATA to PROCESSING
 ALTER TABLE "user" ALTER COLUMN "state" DROP DEFAULT;
-ALTER TABLE "user" ALTER COLUMN "state" TYPE "UserState_new" USING ("state"::text::"UserState_new");
-ALTER TYPE "UserState" RENAME TO "UserState_old";
-ALTER TYPE "UserState_new" RENAME TO "UserState";
-DROP TYPE "UserState_old";
+UPDATE "user" SET "state" = 'PROCESSING' WHERE "state" = 'CRUNCHING_DATA';
+ALTER TABLE "user" ALTER COLUMN "state" TYPE "UserStateNew" USING ("state"::text::"UserStateNew");
+
+ALTER TYPE "UserState" RENAME TO "UserStateOld";
+
+ALTER TYPE "UserStateNew" RENAME TO "UserState";
+
+DROP TYPE "UserStateOld";
+
 ALTER TABLE "user" ALTER COLUMN "state" SET DEFAULT 'AWAITING_CONNECTION';
+
+-- Commit the transaction to apply the changes
 COMMIT;

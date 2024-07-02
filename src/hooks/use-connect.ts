c9772f3c23af6ecb4321465363085fe67438d625
Hashir <progenipx@gmail.com>
2024-07-02 18:08:52 +0100

    fix: lint

--- src/hooks/use-connect.ts
+++ src/hooks/use-connect.ts
@@ -26,8 +26,8 @@ export const useGandalfConnect = () => {
       services: {
         netflix: {
           activities: ["WATCH"],
-        }
-      }
+        },
+      },
     });
 
     return res;
